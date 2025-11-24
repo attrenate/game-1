daa77d068c7aa90c33a663c1a26fd1ae6342f2b1
attrenate <nikolozghomidzevol100@gmail.com>
2025-11-25 00:51:27 +0400

    game logic: done

--- game.js
+++ game.js
@@ -5,15 +5,69 @@ const ctx = canvas.getContext("2d");
 const tileSize = 20;
 const tileCount = canvas.width / tileSize;
 
+// snake starting position 
+let snakeX = 10;
+let snakeY = 10;
 
-// draw first frame
-function drawGame(){
+// snake movement direction
+let velocityX = 1;
+let velocityY = 0;
+
+let speed = 5;
+let lastTime = 0;
+
+// game loop
+function drawGame(time = 0){
+    const secondsSinceLastFrame = (time - lastTime) / 1000;
+    if(secondsSinceLastFrame < 1 / speed){
+        requestAnimationFrame(drawGame);
+        return;
+    }
+    lastTime = time;
+
+    // move snake
+    snakeX += velocityX;
+    snakeY += velocityY;
+
+    // clear canvas
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // draw snake
     ctx.fillStyle = "lime";
-    ctx.fillRect(0, 0, tileSize, tileSize)
+    ctx.fillRect(snakeX * tileSize, snakeY * tileSize, tileSize, tileSize);
 
     requestAnimationFrame(drawGame);
 }
 
 drawGame();
+
+// keyboard controls
+document.addEventListener("keydown", (e) =>{
+    switch(e.key) {
+        case "ArrowUp":
+            if(velocityY !== 1) {
+                velocityX = 0;
+                velocityY = -1;
+            }
+            break;
+        case "ArrowDown":
+            if (velocityY !== -1){
+                velocityX = 0;
+                velocityY = 1;
+            }
+            break;
+        case "ArrowLeft":
+            if (velocityX !== 1) {
+                velocityX = -1;
+                velocityY = 0;
+            }
+            break;
+        case "ArrowRight":
+            if (velocityX !== -1) {
+                velocityX = 1;
+                velocityY = 0;
+            }
+            break;
+    }
+});
